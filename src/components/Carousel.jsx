720144ee89d0cc71c9cb3e6df99989ef78636c85
Pavishni <pavishni.r@gmail.com>
2025-10-25 10:16:16 +0530

    completed carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,59 @@
+import { useEffect, useState } from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { artworks } from "../utils/dataCarousel.js";
 
 const Carousel = () => {
+  const [index, setIndex] = useState(0);
+
+  const nextSlide = () => {
+    setIndex(index === artworks.length - 1 ? 0 : index + 1);
+  };
+
+  const prevSlide = () => {
+    setIndex(index === 0 ? artworks.length - 1 : index - 1);
+  };
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => prev === artworks.length - 1 ? 0 : prev + 1);
+    }, 3000);
+    return () =>clearInterval(interval);
+  }, [artworks.length]);
+
   return (
-    <div>
-      {artworks.map((artwork) => (
-        <div key={artwork.id} className="flex flex-wrap">
-          <img src={artwork.image} alt={artwork.title} className="w-3/4 flex flex-wrap" />
-        </div>
-      ))}
+    <div className="relative w-full max-w-xl mx-auto ">
+      <div className="aspect-[4/3] items-center flex justify-center bg-gray-200 overflow-hidden">
+        <img
+          src={artworks[index]?.image}
+          alt={artworks[index]?.title}
+          className="object-contain h-full w-full transition-opacity duration-700"
+        />
+      </div>
+      <div className="flex justify-between">
+        <button
+          className="absolute left-3 top-1/2 bg-gray-400/40 hover:bg-gray-400/70 rounded-full opacity-100"
+          onClick={prevSlide}
+        >
+          <FiChevronLeft size={24} />
+        </button>
+        <button
+          className="absolute right-3 top-1/2 bg-gray-400/40 hover:bg-gray-400/70 rounded-full opacity-100"
+          onClick={nextSlide}
+        >
+          <FiChevronRight size={24} />
+        </button>
+      </div>
+      <div className="flex justify-center gap-2 my-4 bottom-4">
+        {artworks.map((art, i) => (
+          <button
+            key={i}
+            onClick={() => setIndex(i)}
+            className={`w-2.5 h-2.5 rounded-full transition-all ${
+              index === i ? "bg-gray-800 w-8" : "bg-gray-400  hover:bg-gray-600"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   const { categories } = categoriesData;
   return (
-    <nav className="hidden md:block md:mt-5 bg-[#F8F9FA] fixed top-16 w-full">
+    <nav className="hidden md:block md:mt-5 bg-[#F8F9FA] fixed top-16 w-full z-50">
       <ul className="flex mx-36">
         {categories.map((category, index) => (
           <Link key={category?.catId} to={"/categories/" + category?.catId}>
